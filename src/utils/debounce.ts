5951abbc301d4eb5313a0d284661c7ee6f60b09e
Seokjun.Kang <kangsj0327@gmail.com>
2025-12-11 21:31:10 +0900

    feat: debounce 함수 구현

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -14,13 +14,25 @@
  * @param delay 디바운스 시간 (밀리초)
  * @returns 디바운싱된 새로운 함수
  */
-export function debounce<T extends (...args: any[]) => any>(func: T, delay: number): (...args: Parameters<T>) => void {
+export function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  delay: number
+): (...args: Parameters<T>) => void {
   // 여기에 debounce 로직을 구현하세요.
   // Hint: 클로저를 사용하여 타이머 ID를 관리해야 합니다.
-  return function(...args: Parameters<T>) {
-    // @ts-ignore
-    const context = this;
-    // ...
-    func.apply(context, args);
+  let timerId: number | null = null;
+
+  return function (...args: Parameters<T>) {
+    if (timerId !== null) {
+      clearTimeout(timerId);
+    }
+
+    timerId = setTimeout(() => {
+      func(...args);
+    }, delay);
+
+    // const context = this;
+    // // ...
+    // func.apply(context, args);
   };
 }
