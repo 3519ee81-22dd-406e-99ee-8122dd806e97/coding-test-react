e7a095158eb2840ec8990955149a077ea66b52d5
marte113 <qwerty8739987@gmail.com>
2025-12-12 15:51:42 +0900

    과제2 디바운싱

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -17,10 +17,23 @@
 export function debounce<T extends (...args: any[]) => any>(func: T, delay: number): (...args: Parameters<T>) => void {
   // 여기에 debounce 로직을 구현하세요.
   // Hint: 클로저를 사용하여 타이머 ID를 관리해야 합니다.
-  return function(...args: Parameters<T>) {
+  let timeoutId: ReturnType<typeof setTimeout> | null = null;
+
+  return function(this: any, ...args: Parameters<T>) {
     // @ts-ignore
     const context = this;
-    // ...
+
+    if(timeoutId !== null) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
     func.apply(context, args);
+    timeoutId = null;
+    }, delay)
+    // ...
   };
 }
+
+
+  
